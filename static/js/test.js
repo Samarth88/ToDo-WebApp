73fceb4eb5f61a18070d79d330dc60877f3495ef
Samarth Reddy <samarthreddyryan@gmail.com>
2021-08-07 20:28:58 +0530

    Search for description added

--- static/js/test.js
+++ static/js/test.js
@@ -8,30 +8,14 @@ function myFunction() {
     for (i = 0; i < tr.length; i++) {
         td1 = tr[i].getElementsByTagName("td")[0];
         txtValue1 = td1.textContent || td1.innerText;
-        if (txtValue1.toUpperCase().indexOf(filter) > -1) {
+
+        td2 = tr[i].getElementsByTagName("td")[1];
+        txtValue2 = td2.textContent || td2.innerText;
+
+        if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
             tr[i].style.display = "";
         } else {
             tr[i].style.display = "none";
         }
     }
 }
-/*
-function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
-*/
