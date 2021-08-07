1f2f63b508648655358f9d9662de8536a11903d9
Samarth Reddy <samarthreddyryan@gmail.com>
2021-08-07 20:11:23 +0530

    Adding Search function

--- static/js/test.js
+++ static/js/test.js
@@ -1,1 +1,37 @@
-console.log("Nice")
+
+function myFunction() {
+    var input, filter, tbody, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    tbody = document.getElementById("myTable");
+    tr = tbody.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td1 = tr[i].getElementsByTagName("td")[0];
+        txtValue1 = td1.textContent || td1.innerText;
+        if (txtValue1.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
+/*
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+*/
